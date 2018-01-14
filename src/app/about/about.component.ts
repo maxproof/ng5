727c04f15b072d17348a61c87a982f81a5f9b043
maxproof <max.karberg@gmail.com>
2018-01-14 10:20:23 +0100

    second commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  goals: any;
+
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) { 
+    this.route.params.subscribe(res => console.log(res.id));
+  }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService } from './data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,9 +19,10 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
